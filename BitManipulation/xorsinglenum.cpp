720ce8907e97f5a2e8412d9ecf435c5dbb66383d
Manish <manishkkr071083@gmail.com>
2026-08-18 19:56:29 +0530

    Create xorsinglenum.cpp

---
+++ BitManipulation/xorsinglenum.cpp
@@ -0,0 +1,12 @@
+//136. Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int ele:nums){
+            ans =ans^ele;
+        }
+        return ans;
+    }
+};
